3c647b889ce858da8d21ea9572721a495dbce1e1
Hannes Ebner <hannes@metasolutions.se>
2012-01-23 20:17:26 +0000

    added some missing languages

--- apps/definitions-OE.js
+++ apps/definitions-OE.js
@@ -127,7 +127,8 @@
 	}
   },
   "comments": [{"class": "http://purl.org/stuff/rev#Review", "property": "http://ontologi.es/like#regarding"}],
-  "MPLanguages": [{"value": "", label:{"en":"", "sv":""}},
+  "MPLanguages": [
+  		{"value": "", label:{"en":"", "sv":""}},
 		{"value": "en", label:{"en":"English", "sv":"Engelska"}},
 		{"value": "de", label:{"en":"German", "sv":"Tyska"}},
 		{"value": "fr", label:{"en":"French", "sv":"Franska"}},
@@ -137,7 +138,13 @@
 		{"value": "dk", label:{"en":"Danish", "sv":"Danska"}},
 		{"value": "cz", label:{"en":"Czech", "sv":"Tjeckiska"}},
 		{"value": "it", label:{"en":"Italian", "sv":"Italienska"}},
-		{"value": "sv", label:{"en":"Swedish", "sv":"Svenska"}}],
+		{"value": "sv", label:{"en":"Swedish", "sv":"Svenska"}}
+		{"value": "pl", "label":{"en":"Polish", "sv":"Polska"}},
+		{"value": "hu", "label":{"hu":"Magyar", "en": "Hungarian", "sv": "Ungerska"}},
+		{"value": "tr", "label":{"en":"Turkish", "sv":"Turkiska"}},
+		{"value": "ro", "label":{"ro":"Română", "en": "Romanian", "sv": "Rumänska"}},
+		{"value": "ru", "label":{"ru":"Русский", "en": "Russian", "sv": "Ryska"}}	
+	],
   "MPMap-localMetadata": {
  	"defaultMP": "LOM_OE",
 	"BT": {
