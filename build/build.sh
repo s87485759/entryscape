4f2d1dca2b41cecf428850a226410b388feaba6c
Matthias Palmer <matthias@metasolutions.se>
2012-04-10 20:13:31 +0000

    clean included in release.

--- build/build.sh
+++ build/build.sh
@@ -2,4 +2,4 @@
 
 cd ../lib/dojo-src/util/buildscripts/
 
-./build.sh profileFile=../../../../build/confolio.profile.js action=clean,release version=confolio1.0 releaseName=target localeList=en,en-us,de,el,es,et,hu,no-nb,pt,ro,ru,sv releaseDir=../../../../ cssOptimize=comments.keepLines
+./build.sh profileFile=../../../../build/confolio.profile.js action=release version=confolio1.0 releaseName=target localeList=en,en-us,de,el,es,et,hu,no-nb,pt,ro,ru,sv releaseDir=../../../../ cssOptimize=comments.keepLines
