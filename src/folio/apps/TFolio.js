6be41ef40c3b667ef6bdd0ca0972f837ff5ca53e
matthiaspalmer <mpalmer@gmail.com>
2014-01-06 00:05:07 +0100

    TFolio converted to AMD, dependency to folio.editor.EntryAdminEditorDialog remains to be fixed after it is cleaned up.

--- src/folio/apps/TFolio.js
+++ src/folio/apps/TFolio.js
@@ -1,72 +1,51 @@
-/*
- * Copyright (c) 2007-2010
- *
- * This file is part of Confolio.
- *
- * Confolio is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Confolio is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Confolio. If not, see <http://www.gnu.org/licenses/>.
- */
+/*global define, __confolio*/
+define([
+    "dojo/_base/declare",
+    "dojo/_base/lang",
+    "dojo/_base/connect",
+    "folio/list/ListView",
+    "folio/tree/Tree",
+    "folio/entry/DetailsView",
+    "folio/util/Message",
+    "dijit/layout/BorderContainer",
+    "folio/editor/RFormsEditor",
+    "folio/editor/EntryAdminEditor",
+    "folio/navigation/Breadcrumbs",
+    "folio/util/Widget",
+    "dojo/text!./TFolioTemplate.html"
+], function(declare, lang, connect, ListView, Tree, DetailsView, Message, BorderContainer, RFormsEditorDialog, EntryAdminEditor,
+            Bredcrumbs, Widget, template) {
 
-dojo.provide("folio.apps.TFolio");
-dojo.require("dijit._Widget");
-dojo.require("dijit._Templated");
-dojo.require("folio.list.ListView");
-dojo.require("folio.tree.Tree");
-dojo.require("folio.entry.DetailsView");
-dojo.require("folio.util.Message");
-dojo.require("dijit.layout.BorderContainer");
-dojo.require("folio.create.CreateWizard");
-dojo.require("folio.editor.RFormsEditorDialog");
-dojo.require("folio.editor.EntryAdminEditor");
-dojo.require("folio.navigation.Breadcrumbs");
-dojo.require("folio.create.Create");
-dojo.require("folio.create.Upload");
-dojo.require("folio.create.LinkTo");
-
-
-
-dojo.declare("folio.apps.TFolio", [dijit._Widget, dijit._Templated], {
-	templatePath: dojo.moduleUrl("folio.apps", "TFolioTemplate.html"),
-    widgetsInTemplate: true,
-	startup: function() {
-		//dojo.style(this.detailsDijit.domNode, "width", ""+Math.floor(dojo.marginBox(this.bc.domNode).w/2)+"px");
-		//Hack since using a percentage for details gives a few pixels to little.
-		this.inherited("startup", arguments);
-		this.bc.startup();
-		this.watch("selected", dojo.hitch(this, function() {
-			if (this.get("selected")) {
-				this.listView._list.listenForKeyEvents();
-			} else {
-				this.listView._list.stopListenForKeyEvents();
-			}
-		}));
-	},
-	resize: function() {
-		this.inherited("resize", arguments);
-		this.bc.resize();
-	},
-	/**
-	 * Required by ViewMap to be able to set a nice breadcrumb.
-	 * @param {Object} params
-	 */
-	getLabel: function(params) {
-		return "Default";
-	},
-	show: function(params) {
-		if (params.entry == null && params.context == null) {
-			params.context = this.startContext;
-			params.entry = "_top";
-		}
-		dojo.publish("/confolio/showEntry", [{entry: params.entry, context: params.context, list: params.list}]);		
-	}
+    return declare(Widget, {
+        templateString: template,
+        startup: function() {
+            this.inherited("startup", arguments);
+            this.bc.startup();
+            this.watch("selected", lang.hitch(this, function() {
+                if (this.get("selected")) {
+                    this.listView._list.listenForKeyEvents();
+                } else {
+                    this.listView._list.stopListenForKeyEvents();
+                }
+            }));
+        },
+        resize: function() {
+            this.inherited("resize", arguments);
+            this.bc.resize();
+        },
+        /**
+         * Required by ViewMap to be able to set a nice breadcrumb.
+         * @param {Object} params
+         */
+        getLabel: function(params) {
+            return "Default";
+        },
+        show: function(params) {
+            if (params.entry == null && params.context == null) {
+                params.context = this.startContext;
+                params.entry = "_top";
+            }
+            connect.publish("/confolio/showEntry", [{entry: params.entry, context: params.context, list: params.list}]);
+        }
+    });
 });
