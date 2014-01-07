610089df9917093cc1815a778512d7f7592f64e4
matthiaspalmer <mpalmer@gmail.com>
2014-01-07 10:03:49 +0100

    Comment code now in AMD form.

--- src/folio/comment/Comment.js
+++ src/folio/comment/Comment.js
@@ -1,64 +1,52 @@
-/*
- * Copyright (c) 2007-2010
- *
- * This file is part of Confolio.
- *
- * Confolio is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Confolio is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Confolio. If not, see <http://www.gnu.org/licenses/>.
- */
+/*global define, __confolio*/
+define([
+    "dojo/_base/declare",
+    "dojo/_base/lang",
+    "dojo/dom-attr",
+    "dojo/_base/connect",
+    "folio/editor/RFormsPresenter",
+    "folio/util/Widget",
+    "dijit/Editor",
+    "dojo/text!./CommentTemplate.html"
+], function(declare, lang, attr, connect, RFormsPresenter, Widget, Editor, template) {
 
-dojo.provide("folio.comment.Comment");
-dojo.require("folio.editor.RFormsPresenter");
-dojo.require("dijit.Editor");
+    return declare(Widget, {
+        templateString: template,
+        entry: null,
 
-
-dojo.declare("folio.comment.Comment", [dijit._Widget, dijit._Templated], {
-	widgetsInTemplate: true,
-	templatePath: dojo.moduleUrl("folio.comment", "CommentTemplate.html"),
-	entry: null,
-
-	/**
- 	* Constructor. 
- 	* Does the initial localization and sets the default text for the text area.
- 	* It also creates the dialog box that actually shows the content.
- 	* @param {Object} args - The entry which comments are to be loaded and 
- 	* the application (which is, at least, 
- 	* needed for retrieving the home context for the user)
- 	*/
-	constructor: function(args) {
-		this.name = args.name;
-		this.date = args.date;
-		this.time = args.time;
-		this.application = __confolio.application;
-		//this.content = args.content;
-	},
-	postCreate: function() {
-		this.inherited("postCreate", arguments);
-		this.presenterDijit.show(this.entry, false);
-		var cre = this.entry.getCreationDate();
-		if (cre != null) {
-			dojo.attr(this.timeNode, "innerHTML", cre);
-		}
-		var creator = this.entry.getCreator();
-		if (creator != null) {
-			this.application.getStore().loadEntry(creator, {}, dojo.hitch(this, function(ent) {
-				dojo.attr(this.creatorNode, "innerHTML", folio.data.getLabel(ent));
-			}),dojo.hitch(this, function(mesg) {
-				dojo.attr(this.creatorNode, "innerHTML", "Unknown");
-			}));
-		} else {
-			dojo.attr(this.creatorNode, "innerHTML", "Unknown");
-		}
-		this.contentViewDijit.show(this.entry);
-	}
+        /**
+         * Constructor.
+         * Does the initial localization and sets the default text for the text area.
+         * It also creates the dialog box that actually shows the content.
+         * @param {Object} args - The entry which comments are to be loaded and
+         * the application (which is, at least,
+         * needed for retrieving the home context for the user)
+         */
+        constructor: function(args) {
+            this.name = args.name;
+            this.date = args.date;
+            this.time = args.time;
+            this.application = __confolio.application;
+            //this.content = args.content;
+        },
+        postCreate: function() {
+            this.inherited("postCreate", arguments);
+            this.presenterDijit.show(this.entry, false);
+            var cre = this.entry.getCreationDate();
+            if (cre != null) {
+                attr.set(this.timeNode, "innerHTML", cre);
+            }
+            var creator = this.entry.getCreator();
+            if (creator != null) {
+                this.application.getStore().loadEntry(creator, {}, lang.hitch(this, function(ent) {
+                    attr.set(this.creatorNode, "innerHTML", folio.data.getLabel(ent));
+                }),lang.hitch(this, function(mesg) {
+                    attr.set(this.creatorNode, "innerHTML", "Unknown");
+                }));
+            } else {
+                attr.set(this.creatorNode, "innerHTML", "Unknown");
+            }
+            this.contentViewDijit.show(this.entry);
+        }
+    });
 });
