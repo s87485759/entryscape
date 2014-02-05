ab2234e7e74df2314f46791aad6cbce4306d8c4c
matthiaspalmer <mpalmer@gmail.com>
2014-02-05 16:18:17 +0100

    Disabled old administrate dialog, support for changing existing links and replacing files will need to be reintroduced.

--- src/folio/list/List.js
+++ src/folio/list/List.js
@@ -505,7 +505,7 @@ dojo.declare("folio.list.List", [folio.list.AbstractList, dijit.layout._LayoutWi
 		if (__confolio.config["possibleToCommentEntry"] === "true") {
 			o.push({action: "comment", enabled: true, label: this.resourceBundle.comment}); //is not a system entry
 		}
-		o.push({action: "admin", enabled: child.possibleToAdmin(), label: this.resourceBundle.admin});
+		//o.push({action: "admin", enabled: child.possibleToAdmin(), label: this.resourceBundle.admin});
         o.push({action: "rights", enabled: child.possibleToAdmin(), label: this.resourceBundle.rights});
 		o.push({action: "remove", enabled: (child.possibleToAdmin() && this.list.isResourceModifiable() && this.list.isMetadataModifiable), label: (isTrashFolder ? this.resourceBundle.empty : this.resourceBundle.remove)});
 		o.push({action: "copy", enabled: (child.isMetadataAccessible() && child.isResourceAccessible() && !isTrashFolder), label: this.resourceBundle.copy}); //ChildMD and Resource is accessible
